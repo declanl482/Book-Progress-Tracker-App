1081e695dbe2e5e6e07d042460d2443e742dc05c
Declan <declanl482@gmail.com>
2023-07-16 13:46:34 -0400

    restructured config package to separate testing and app configurations

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,18 +17,7 @@ type Config struct {
 	AccessTokenSecretKey string
 }
 
-type TConfig struct {
-	TestDatabaseHostname     string
-	TestDatabasePort         string
-	TestDatabaseName         string
-	TestDatabaseUsername     string
-	TestDatabasePassword     string
-	TestDatabaseTimezone     string
-	TestAccessTokenSecretKey string
-}
-
 var AppConfig Config
-var TestConfig TConfig
 
 func LoadApplicationConfigurationVariables() error {
 	err := godotenv.Load("C:/Users/13dli/go/src/github.com/declanl482/go-book-tracker-app/.env")
@@ -45,24 +34,6 @@ func LoadApplicationConfigurationVariables() error {
 		DatabasePassword:     os.Getenv("DATABASE_PASSWORD"),
 		DatabaseTimezone:     os.Getenv("DATABASE_TIMEZONE"),
 		AccessTokenSecretKey: os.Getenv("ACCESS_TOKEN_SECRET_KEY"),
-	}
-	return nil
-}
-
-func LoadTestingConfigurationVariables() error {
-	err := godotenv.Load("C:/Users/13dli/go/src/github.com/declanl482/go-book-tracker-app/.env.test")
-	if err != nil {
-		return fmt.Errorf("error in function LoadTestingConfigurationVariables ; failed to load .env file: \n%v", err)
-	}
-
-	TestConfig = TConfig{
-		TestDatabaseHostname:     os.Getenv("TEST_DATABASE_HOSTNAME"),
-		TestDatabasePort:         os.Getenv("TEST_DATABASE_PORT"),
-		TestDatabaseName:         os.Getenv("TEST_DATABASE_NAME"),
-		TestDatabaseUsername:     os.Getenv("TEST_DATABASE_USERNAME"),
-		TestDatabasePassword:     os.Getenv("TEST_DATABASE_PASSWORD"),
-		TestDatabaseTimezone:     os.Getenv("TEST_DATABASE_TIMEZONE"),
-		TestAccessTokenSecretKey: os.Getenv("TEST_ACCESS_TOKEN_SECRET_KEY"),
 	}
 	return nil
 }
---
+++ internal/config/testing_config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type TestingConfig struct {
+	TestDatabaseHostname     string
+	TestDatabasePort         string
+	TestDatabaseName         string
+	TestDatabaseUsername     string
+	TestDatabasePassword     string
+	TestDatabaseTimezone     string
+	TestAccessTokenSecretKey string
+}
+
+var TestConfig TestingConfig
+
+func LoadTestingConfigurationVariables() error {
+	err := godotenv.Load("C:/Users/13dli/go/src/github.com/declanl482/go-book-tracker-app/.env.test")
+	if err != nil {
+		return fmt.Errorf("error in function LoadTestingConfigurationVariables ; failed to load .env file: \n%v", err)
+	}
+
+	TestConfig = TestingConfig{
+		TestDatabaseHostname:     os.Getenv("TEST_DATABASE_HOSTNAME"),
+		TestDatabasePort:         os.Getenv("TEST_DATABASE_PORT"),
+		TestDatabaseName:         os.Getenv("TEST_DATABASE_NAME"),
+		TestDatabaseUsername:     os.Getenv("TEST_DATABASE_USERNAME"),
+		TestDatabasePassword:     os.Getenv("TEST_DATABASE_PASSWORD"),
+		TestDatabaseTimezone:     os.Getenv("TEST_DATABASE_TIMEZONE"),
+		TestAccessTokenSecretKey: os.Getenv("TEST_ACCESS_TOKEN_SECRET_KEY"),
+	}
+	return nil
+}
